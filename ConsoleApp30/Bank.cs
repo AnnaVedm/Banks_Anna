6042ea262c68d7831fb67738d9206cc2096a2154
Anna <Anna@DESKTOP-MH56V0K>
2023-12-05 17:49:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp30/Bank.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Анюткабанки
+{
+    class Bank
+    {
+        private int id;
+        private string FIO;
+        private int schet_number;
+        private double dengi;
+
+        public void Information(int id, string FIO, int schet_number, double dengi)
+        {
+            this.id = id;
+            this.FIO = FIO;
+            this.schet_number = schet_number;
+            this.dengi = dengi;
+        }
+
+        public void schet_vybor(Bank[] schet, int j, int choice)//Вывод всех счетов
+        {
+            for (int i = 0; i < schet.Length; i++)
+            {
+                if (i == j) //Заход в аккаунт
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"{schet[i].id}. {schet[i].FIO}, {schet[i].schet_number}");
+                }
+            }
+        }
+
+        public void vybory(Bank[] schet, int actionChoice, int accountIndex, int choice)
+        {
+            switch (actionChoice)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Вы выбрали положить деньги на счёт");
+                    schet[accountIndex].dob(schet, accountIndex);
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Вы выбрали снять деньги со счёта");
+                    schet[accountIndex].umen(schet, accountIndex);
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Вы выбрали обнулить счёт.");
+                    schet[accountIndex].obnul(schet, accountIndex);
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Вы выбрали перевести сумму с одного счёта на другой");
+                    Console.WriteLine("Выберите счет для перевода: ");
+                    schet_vybor(schet, accountIndex, choice);//Вывод всех счетов
+
+                    int chooseToSend = Convert.ToInt32(Console.ReadLine());
+                    int massiv_index = chooseToSend - 1;
+
+                    schet[accountIndex].Perevod(schet, actionChoice, accountIndex, massiv_index);
+                    break;
+
+                case 5:
+                    Console.WriteLine("Вы выбрали выйти");
+                    Console.Clear();
+                    return;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Некорректный выбор.");
+                    break;
+            }
+        }
+
+        private void Perevod(Bank[] accounts, int vybor, int schetIndex, int indexKonec)
+        {
+            Console.Write("Укажите, сколько хотите перевести: ");
+            double sendMoney = Convert.ToDouble(Console.ReadLine());
+
+            while (sendMoney < 0 || accounts[schetIndex].dengi < sendMoney)
+            {
+                Console.WriteLine("На вашем счетe не хватает денег. Попробуйте снова:");
+                sendMoney = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"Отправлено {sendMoney} руб");
+
+            Console.WriteLine();
+
+            Console.Write("Нажмите любую клавишу для вывода информации о счетах: ");
+            Console.ReadKey();
+            Console.Clear();
+
+            accounts[schetIndex].dengi -= sendMoney; //Перевод
+            accounts[indexKonec].dengi += sendMoney; //Получение
+
+            Console.WriteLine("Информация о счете, на который переводились деньги: ");
+            accounts[indexKonec].Otkryt_schet();
+
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.WriteLine("Информация о счете, с которого переводились деньги: ");
+            accounts[schetIndex].Otkryt_schet();
+
+            Console.WriteLine();
+        }
+
+        private void obnul(Bank[] schet, int schetIndex)
+        {
+            schet[schetIndex].dengi = 0;
+            Console.WriteLine($"теперь на вашем счете {schet[schetIndex].dengi} руб");
+
+            Console.WriteLine();
+        }
+
+        private void dob(Bank[] schet, int schetIndex)
+        {
+            Console.Write("Укажите сумму для пополнения: ");
+            double dob = Convert.ToDouble(Console.ReadLine());
+
+            while (dob <= 0)
+            {
+                Console.WriteLine("Нельзя перевести отрицательное количество денег или равное 0. Попробуйте снова:");
+                dob = Convert.ToDouble(Console.ReadLine());
+            }
+
+            schet[schetIndex].dengi += dob;
+            Console.WriteLine($"Теперь на счетe {schet[schetIndex].dengi} руб");
+
+            Console.WriteLine();
+        }
+
+        public void umen(Bank[] schet, int schet_index)
+        {
+            Console.Clear();
+            Console.Write("Сколько денег вы хотите вывести со счёта?: ");
+            double minus = Convert.ToDouble(Console.ReadLine());
+
+            while (minus > schet[schet_index].dengi)
+            {
+                Console.WriteLine("У вас не хватает денег для вывода. Попробуйте выбрать сумму поменьше.");
+                minus = Convert.ToDouble(Console.ReadLine());
+            }
+
+            schet[schet_index].dengi -= minus;
+
+            Console.WriteLine($"Со счета выведено {minus} руб");
+            Console.WriteLine($"Осталось {schet[schet_index].dengi} руб");
+
+            Console.WriteLine();
+        }
+
+        public void Otkryt_schet()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"ФИО: {FIO}");
+            Console.WriteLine($"Номер счета: {schet_number}");
+            Console.WriteLine($"Средств на счете: {dengi} руб");
+        }
+    }
+}
---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Анюткабанки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string FIO;
+            int accountNumber; //номер счета
+            double dengi;
+
+            Console.Write("Здравствуйте! Мы приветствуем вас в банке Анютка. Спасибо, что выбрали нас!");
+            Console.Write("\nУкажите количество клиентов банка: ");
+            int schetnumb = Convert.ToInt32(Console.ReadLine());
+
+            Random rnd = new Random();
+
+            Bank[] schet = new Bank[schetnumb];
+            Bank bank = new Bank();
+
+            Console.WriteLine();
+            for (int i = 0; i < schet.Length; i++)
+            {
+                Console.WriteLine($"Клиент {i + 1}");
+                schet[i] = new Bank();
+
+                id = i + 1;
+
+                Console.Write("Введите ваше ФИО: ");
+                FIO = Console.ReadLine();
+
+                accountNumber = rnd.Next(123455600, 999927839);
+
+                Console.Write("Укажите, сколько денег хотите положить на счет: ");
+                dengi = Convert.ToDouble(Console.ReadLine());
+
+                schet[i].Information(id, FIO, accountNumber, dengi);
+
+                Console.WriteLine("\nИнформация о счете: ");
+                schet[i].Otkryt_schet();
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Выберите счёт, на который хотите зайти (Для выхода укажите 0): ");
+                bank.schet_vybor(schet, schet.Length, schet.Length);
+
+                int vybor = Convert.ToInt32(Console.ReadLine());
+
+                if (vybor == 0)
+                {
+                    Console.WriteLine("Конец программы");
+                    break;
+                }
+                else
+                {
+                    int schet_index = vybor - 1;
+
+                    while (vybor > schet.Length || schet_index < 0)
+                    {
+                        Console.WriteLine("Такого счёта не существует. Попробуйте ввести снова: ");
+                        vybor = Convert.ToInt32(Console.ReadLine());
+                        schet_index = vybor - 1;                //Номер счёта как индекс в массиве
+                    }
+
+                    schet[schet_index].Otkryt_schet();
+
+                    Console.WriteLine("\nВыберите действие:\n1.Положить деньги на счёт\n2.Снять деньги со счёта\n3.Обнулить счёт\n4.Перевести сумму с одного счёта на другой.\n5.Выход");
+                    int actionChoice = Convert.ToInt32(Console.ReadLine());
+
+                    schet[schet_index].vybory(schet, actionChoice, schet_index, vybor);
+                }
+            }
+        }
+
+        static void output(Bank[] accounts)
+        {
+            for (int i = 0; i < accounts.Length; i++) //Вывод каждого счёта
+            {
+                Console.WriteLine($"Счёт {i + 1}");
+                accounts[i].Otkryt_schet();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
